3209f5840811977097aa75fa2cd198636fc911c5
AyubIRZ <ayubirazeh@gmail.com>
2020-06-16 00:32:00 +0430

    update Category model, add mass assignable fields and Product relation

--- app/app/Category.php
+++ app/app/Category.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+
+    /**
+     * Relation with Product model that returns related Products.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function category()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
