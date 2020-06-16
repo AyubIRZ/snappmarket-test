cf8f533226ac1c1d0eac4cd91544aa470edfbc59
AyubIRZ <ayubirazeh@gmail.com>
2020-06-17 02:42:27 +0430

    add routes for jwt API authentication

--- app/routes/api.php
+++ app/routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'middleware' => 'api',
+    'namespace' => 'API\Auth',
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
