25c28141f51a4a5483c30ea5c701193d74f2095c
AyubIRZ <ayubirazeh@gmail.com>
2020-06-16 15:34:56 +0430

    add index method to ProductController to show product list paginated

--- app/app/Http/Controllers/API/ProductController.php
+++ app/app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::with('category')->paginate(10);
+
+        $response = [
+            'ok' => true,
+            'message' => 'Successfully retrieved products!',
+            'data' => $products
+        ];
+
+        return response()->json($response, 200);
+    }
 }
