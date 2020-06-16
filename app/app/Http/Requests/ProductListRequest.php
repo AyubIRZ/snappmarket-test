14dc80d35d38f4133f9f4f3261827d88b9367085
AyubIRZ <ayubirazeh@gmail.com>
2020-06-17 00:31:30 +0430

    override the default ProductListRequest validation response

--- app/app/Http/Requests/ProductListRequest.php
+++ app/app/Http/Requests/ProductListRequest.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
 
 class ProductListRequest extends FormRequest
 {
@@ -26,5 +29,42 @@ class ProductListRequest extends FormRequest
         return [
             'category' => 'nullable|integer|exists:categories,id'
         ];
+    }
+
+    /**
+     * Set the response object that will be returned if any validation fails.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'category.integer' => 'The category value should be an integer.',
+            'category.exists' => 'The selected category was not found.',
+        ];
+    }
+
+    /**
+     * Override the default error response for validation.
+     *
+     * @param $validator
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function buildResponse($validator)
+    {
+        return response()->json([
+            'ok' => false,
+            'message' => 'There are problems retrieving products!',
+            'errors' => $validator->errors()->all()
+        ], 422);
+    }
+
+    /**
+     * @param Validator $validator
+     * @throws ValidationException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw (new ValidationException($validator, $this->buildResponse($validator)));
     }
 }
